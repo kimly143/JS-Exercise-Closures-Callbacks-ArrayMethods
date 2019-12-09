ac88dc536be891a35b0f9b1c35b74f9cf8bf8be2
Kim Ly Buck <kim.3993@gmail.com>
2019-12-09 14:28:02 -0600

    finished

--- index.js
+++ index.js
@@ -308,9 +308,22 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(max) {
+  let count = 0;
+  return function counter(){
+      if (count < max){
+        return count++;
+      }
+      else if (count === max){
+        count =0;
+        return max;
+      }
+      else {
+        return count=0;
+      }
+    }
 }
+
 
 /////////////// END OF CHALLENGE ///////////////
 /////////////// END OF CHALLENGE ///////////////
